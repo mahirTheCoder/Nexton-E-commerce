038bd910f352f02770bec03caadfb2e9404c1b7f
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-09-19 00:39:48 +0600

    return

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -25,7 +25,25 @@ const Hero = () => {
               </div>
             </div>
 
-          
+            {/* -----------return--------- */}
+            <div className="return flex gap-3">
+              <div className="border-l-[3px] h-12.5 border-[#E5E7EB]"></div>
+              <div className="shipping flex gap-5 items-center">
+                <div className="img w-6 h-6">
+                  <img src={hero2} alt="hero1" />
+                </div>
+                <div className="content">
+                  <h2 className="text-xl text-primery font-medium font-poppins">
+                    Very easy to return
+                  </h2>
+                  <p className="text-sm text-[#4B5563] font-medium font-poppins">
+                    Just phone number
+                  </p>
+                </div>
+              </div>
+            </div>
+
+      
         </div>
       </section>
     </>
