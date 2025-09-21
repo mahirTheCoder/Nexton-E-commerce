a9a9f2b2e107eb68e3b942691a9bb272e4c6eda0
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-09-21 21:55:46 +0600

    txt part

--- src/Components/RecCArt.jsx
+++ src/Components/RecCArt.jsx
@@ -9,13 +9,15 @@ const RecCArt = () => {
         common2={"Best matching products for you"}
       />
 
-      <div className="recommenmded-content w-[344px] h-[448px]">
-        <div className="img-part w-full h-[347px] bg-gray-300 rounded-[16px] mt-5">
-          <img src={""} alt="img" />
+      <div className="recommenmded-content w-[344px] h-[448px] mt-10">
+        <div className="img-part w-full h-[347px] bg-gray-300 rounded-[16px] mb-5">
+          <img src={"/"} alt="img" />
         </div>
-        
-
-      </div>
+        <div className="title-price flex justify-between mb-1">
+          <h2 className="text-base font-semibold font-poppins text-primery ">Black Automatic Watch</h2>
+          <h2 className="text-base font-semibold font-poppins text-primery ">$169.99</h2>
+        </div>
+     
     </>
   );
 };
