f349d5dbfe2408a0d37912e8b26b04310cb704d7
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-09-26 23:16:31 +0600

    manu cart login add

--- src/Components/Responsivenav.jsx
+++ src/Components/Responsivenav.jsx
@@ -3,7 +3,8 @@ import favicon from "../assets/Images/favicon.png";
 import { Link } from "react-router";
 import { CiSearch } from "react-icons/ci";
 import { IoMdMenu } from "react-icons/io";
-
+import { RiUser3Line } from "react-icons/ri";
+import { IoCartOutline } from "react-icons/io5";
 const Responsivenav = () => {
   return (
     <>
@@ -19,13 +20,25 @@ const Responsivenav = () => {
               <button className="pl-4">
                 <CiSearch className="text-sm text-primery" />
               </button>
-              <input type="text" placeholder="Search in products..."  className="text-sm font-normal font-poppins text-primery border-none outline-none"/>
+              <input
+                type="text"
+                placeholder="Search in products..."
+                className="text-sm font-normal font-poppins text-primery border-none outline-none"
+              />
             </div>
             <div className="buttons text-2xl text-primery">
               <IoMdMenu />
-
             </div>
           </div>
+        </div>
+
+        <div className="cart-button w-full bg-white py-4 px-4 flex justify-center  items-center gap-2">
+          <button>
+            <RiUser3Line className="text-2xl text-primery"/>
+            </button>
+          <button>
+            <IoCartOutline className="text-2xl text-primery"/>
+            </button>
         </div>
       </nav>
     </>
