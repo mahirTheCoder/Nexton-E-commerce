26b1b5a576f04ad83431b987a7d418da4167dd22
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-09-26 20:47:41 +0600

    search bar

--- src/Components/Responsivenav.jsx
+++ src/Components/Responsivenav.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import favicon from '../assets/Images/favicon.png'
-import { Link } from 'react-router'
+import React from "react";
+import favicon from "../assets/Images/favicon.png";
+import { Link } from "react-router";
+import { CiSearch } from "react-icons/ci";
+
 const Responsivenav = () => {
   return (
     <>
-    <nav className='lg:hidden block'>
-      <div className="container">
-        <div className="res-row">
-          <div className="logo">
-            <Link className='w-6' to={'/'} ><img  src={favicon} alt="logo" /></Link>
+      <nav className="lg:hidden block">
+        <div className="container px-6">
+          <div className="res-row mb-10 flex justify-between items-center py-5">
+            <div className="logo">
+              <Link className="w-6" to={"/"}>
+                <img src={favicon} alt="logo" />
+              </Link>
+            </div>
+            <div className="search-buton rounded-[9999px]  bg-[#F8F8F8] flex gap-2 items-center  w-[200px] h-[36px] ">
+              <button className="pl-4">
+                <CiSearch className="text-sm text-primery" />
+              </button>
+              <input type="text" placeholder="Search in products..."  className="text-sm font-normal font-poppins text-primery border-none outline-none"/>
+            </div>
+            <div className="buttons"></div>
           </div>
-          <div className="search-buton"></div>
-          <div className="buttons"></div>
         </div>
-      </div>
-    </nav>
-    
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Responsivenav
+export default Responsivenav;
