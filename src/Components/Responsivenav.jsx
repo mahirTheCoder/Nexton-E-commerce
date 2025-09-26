43b913f449b42d38674a78b9520358d391084511
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-09-26 20:06:27 +0600

    responsive nav add

---
+++ src/Components/Responsivenav.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Responsivenav = () => {
+  return (
+    <>
+    <nav>
+      <div className="container">
+        <div className="res-row">
+          <div className="logo"></div>
+          <div className="search-buton"></div>
+          <div className="buttons"></div>
+        </div>
+      </div>
+    </nav>
+    
+    </>
+  )
+}
+
+export default Responsivenav
