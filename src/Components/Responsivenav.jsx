b839bb52b3226fa4f8174106d58c47cc5e46d9ee
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-09-26 20:18:38 +0600

    logo add

--- src/Components/Responsivenav.jsx
+++ src/Components/Responsivenav.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-
+import favicon from '../assets/Images/favicon.png'
+import { Link } from 'react-router'
 const Responsivenav = () => {
   return (
     <>
-    <nav>
+    <nav className='lg:hidden block'>
       <div className="container">
         <div className="res-row">
-          <div className="logo"></div>
+          <div className="logo">
+            <Link className='w-6' to={'/'} ><img  src={favicon} alt="logo" /></Link>
+          </div>
           <div className="search-buton"></div>
           <div className="buttons"></div>
         </div>
--- src/Layouts/LaoutOne.jsx
+++ src/Layouts/LaoutOne.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Outlet } from 'react-router'
 import Navbar from '../Components/Navbar'
 import Footer from '../Components/Footer'
+import Responsivenav from '../Components/Responsivenav'
 
 const LaoutOne = () => {
   return (
     <>
     <Navbar/>
+    <Responsivenav/>
     <Outlet/>
     <Footer/>
     </>
