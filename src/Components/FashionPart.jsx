6a058f8a2a45a8cd2b151f989e4a9c73e5885375
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-09-29 00:04:39 +0600

    api fetching

--- src/Components/FashionPart.jsx
+++ src/Components/FashionPart.jsx
@@ -1,26 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CommonHead from "./CommonHead";
 import FashinCart from "./FashinCart";
+import axios from "axios";
 
 const FashionPart = () => {
+  const [showAll, setShowAll] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://dummyjson.com/products")
+      .then((res) => setShowAll(res.data.products))
+      .catch((err) => console.log(err));
+  }, []);
+
+  useEffect(() => {
+    console.log(showAll);
+  }, [showAll]);
   return (
     <>
-      <section id="fashion" className="mt-[176px]">
-        <div className="container">
-          <div className="header-part mb-10">
-           <CommonHead
-              common1={"Best Sellers."}
-              common2={"Best selling of the month"}
-            />
-          </div>
-          <div className="api-part">
-            <FashinCart />
-          </div>
-        </div>
-      </section>
+
     </>
   );
 };
 
 export default FashionPart;
- 
