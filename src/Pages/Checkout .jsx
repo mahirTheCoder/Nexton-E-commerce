78c503b5d7116a3f6649dc0474a49baac11a9cc2
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-10-01 19:52:08 +0600

    cheakout

--- src/Pages/Checkout .jsx
+++ src/Pages/Checkout .jsx
@@ -3,9 +3,7 @@ import React from 'react'
 const Checkout  = () => {
   return (
     <>
-    <div className="container">
-      <h2 className='text-2xl font-medium text-red-400'> Hey I'm Here</h2>
-    </div>
+  
      </>
   )
 }
