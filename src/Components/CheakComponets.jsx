fd3f2cce2722075209dabee79be515cc42f44daa
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-10-01 20:11:06 +0600

    button design complete

--- src/Components/CheakComponets.jsx
+++ src/Components/CheakComponets.jsx
@@ -6,7 +6,17 @@ const CheakComponets = () => {
       <section id="cheakout " className="mt-10">
         <div className="container">
           <div className="img-and-content flex gap-10">
-              <div className="imgpart lg:w-[804px] h-[690px] bg-amber-300 "></div>
+              <div className="imgpart lg:w-[804px] h-[690px] bg-amber-300 ">
+                <div className="button-img flex justify-between">
+                  <div className="button flex flex-col">
+                    <button className="w-[140px] h-[158px] bg-amber-800 mb-4 overflow-hidden rounded-[16px]"></button>
+                    <button className="w-[140px] h-[158px] bg-amber-800 mb-4 overflow-hidden rounded-[16px]"></button>
+                    <button className="w-[140px] h-[158px] bg-amber-800 mb-4 overflow-hidden rounded-[16px]"></button>
+                    <button className="w-[140px] h-[158px] bg-amber-800  overflow-hidden rounded-[16px]"></button>
+                  </div>
+                  <div className="img"></div>
+                </div>
+              </div>
               <div className="addt-to-cart w-[460px] h-[463px] bg-amber-200 "></div>
           </div>
         </div>
