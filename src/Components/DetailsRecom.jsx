818bf8eb82cf5debf91c12a70ea6e293cc90bc8f
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-10-07 20:18:48 +0600

    price part

--- src/Components/DetailsRecom.jsx
+++ src/Components/DetailsRecom.jsx
@@ -6,8 +6,14 @@ const DetailsRecom = () => {
       <div className="DetailsRecom">
         <div className="container">
           <div className="div w-[309px] h-[448px] bg-amber-50 rounded-[16px]">
-            <div className="w-[309px] h-[360px] bg-slate-300 ">
-              <img src={""} alt="" />
+            {/* --------------img part-------- */}
+            <div className="w-[309px] h-[360px] bg-slate-300 mb-3 rounded-[16px]">
+              <img src={""} alt="img" />
+            </div>
+            {/* ----------prodict name and pricr ---- */}
+            <div className="">
+              <h2 className="text-base font-semibold font-poppins text-primery ">Black Automatic Watch</h2>
+              <h2 className="text-base font-semibold font-poppins text-primery ">$169.99</h2>
             </div>
           </div>
         </div>
