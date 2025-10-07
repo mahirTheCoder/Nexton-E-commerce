88098b72d44818df5e3cd016d5ef17306a9e263e
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-10-07 20:11:26 +0600

    design part

--- src/Components/DetailsRecom.jsx
+++ src/Components/DetailsRecom.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const DetailsRecom = () => {
   return (
     <>
-    
+      <div className="DetailsRecom">
+        <div className="container">
+          <div className="div w-[309px] h-[448px] bg-amber-50 rounded-[16px]">
+            <div className="w-[309px] h-[360px] bg-slate-300 ">
+              <img src={""} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default DetailsRecom
+export default DetailsRecom;
--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import CheakComponets from '../Components/CheakComponets'
+import DetailsRecom from '../Components/DetailsRecom'
 const ProductPage = () => {
 
   
   return (
     <>
     <CheakComponets />
+    <DetailsRecom />
     </>
   )
 }
