09249904765835e3182feefc4c9218c25d83846d
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-10-24 20:54:55 +0600

    connect use location all product page

--- src/Components/AllProducts.jsx
+++ src/Components/AllProducts.jsx
@@ -1,12 +1,16 @@
 import axios from "axios";
 import React from "react";
 import BredCrums from "./BredCrums";
+import { useLocation } from "react-router";
 
 const AllProducts = () => {
   axios
     .get("https://dummyjson.com/products?limit=15&skip=10&select=title,price")
     .then((res) => console.log(res))
     .catch((err) => console.log(err));
+
+  const myLocation = useLocation();
+  console.log(myLocation.pathname.split("/")[1]);
 
   return (
     <>
--- src/Components/BredCrums.jsx
+++ src/Components/BredCrums.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { FaHome } from "react-icons/fa";
-import { Link, useLocation } from "react-router";
+import { Link } from "react-router";
 
-const BredCrums = () => {
-const myLocation = useLocation()
-console.log(myLocation);
+const BredCrums = ({pagename , pagelink}) => {
+
 
   return (
     <>
       <div className="bredcrums">
         <Link className="text-lg  font-normal flex items-center gap-2 " to={"/"}><FaHome /> /</Link>
+        <Link className="text-lg  font-normal flex items-center gap-2 " to={pagelink}>{pagename}</Link>
       </div>
     </>
   );
