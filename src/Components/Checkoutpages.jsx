92379cd7a79e7dc2b0810e74aa0448d9ddc1da76
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-11-30 19:34:20 +0600

    shipping address

--- src/Components/Checkoutpages.jsx
+++ src/Components/Checkoutpages.jsx
@@ -41,7 +41,64 @@ export default function Checkoutpages() {
             </div>
           </div>
 
-   
+          <div className="border border-gray-200 bg-white p-8 rounded-xl shadow-sm">
+            <div className="flex items-center gap-2 mb-4">
+              <FaTruck className="text-[var(--color-primery)]" />
+              <h2 className="font-semibold text-lg text-[var(--color-primery)]">
+                Shipping Address
+              </h2>
+            </div>
+
+            <div className="grid sm:grid-cols-2 gap-8">
+              <input
+                type="text"
+                placeholder="First name"
+                className="border border-gray-200 outline-none p-3 rounded-lg"
+              />
+              <input
+                type="text"
+                placeholder="Last name"
+                className="border border-gray-200 outline-none p-3 rounded-lg"
+              />
+              <input
+                type="text"
+                placeholder="Address line 1"
+                className="border border-gray-200 outline-none p-3 rounded-lg"
+              />
+              <input
+                type="text"
+                placeholder="Apt, Suite"
+                className="border border-gray-200 outline-none p-3 rounded-lg"
+              />
+              <input
+                type="text"
+                placeholder="Address line 2"
+                className="border border-gray-200 outline-none p-3 rounded-lg sm:col-span-2"
+              />
+              <input
+                type="text"
+                placeholder="City"
+                className="border border-gray-200 outline-none p-3 rounded-lg"
+              />
+              <input
+                type="text"
+                placeholder="Country"
+                className="border border-gray-200 outline-none p-3 rounded-lg"
+              />
+              <input
+                type="text"
+                placeholder="State/Province"
+                className="border border-gray-200 outline-none p-3 rounded-lg"
+              />
+              <input
+                type="text"
+                placeholder="Postal code"
+                className="border border-gray-200 outline-none p-3 rounded-lg"
+              />
+            </div>
+          </div>
+
+     
         </div>
 
 
