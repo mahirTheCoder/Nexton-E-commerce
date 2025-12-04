c1aae0fe846db4650c49cb0ffc1edcb52db188d1
mahirTheCoder <mahirchowdhury518@gmail.com>
2025-12-04 20:34:11 +0600

    about 2

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -36,7 +36,27 @@ const About = () => {
           </div>
         </div>
 
-   
+        {/* --------------------- BLOCK 2 --------------------- */}
+        <div className="grid grid-cols-2 gap-12 items-center mb-20">
+
+          {/* Text Content */}
+          <div>
+            <h3 className="text-lg font-semibold">Provide fashionable and qualified products</h3>
+            <p className="text-sm text-gray-600 mt-3 leading-6">
+              Already millions of people are very satisfied by this page builder and the
+              number is growing more and more. Technology developing, requirements are
+              increasing. Riode has brought.
+            </p>
+          </div>
+
+          {/* Image Placeholder Box */}
+          <div
+            className="bg-gray-200 rounded-xl"
+            style={{ width: "622px", height: "506px" }}
+          ></div>
+        </div>
+
+    
       </section>
     </>
   );
